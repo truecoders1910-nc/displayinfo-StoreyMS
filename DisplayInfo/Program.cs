68b475af4f1b9a6bec71e1d20adb1cb991a1b835
Matt Storey <storeyms2010@gmail.com>
2019-11-01 18:17:45 -0500

    Final Draft 1.1 (Without all the white space)

--- DisplayInfo/Program.cs
+++ DisplayInfo/Program.cs
@@ -37,6 +37,23 @@ namespace DisplayInfo
             string animal = Console.ReadLine();
 
             Console.WriteLine($"Great! So {name}, your favorite number is {num1} and your favorite animal is a {animal}!");
+
+            // Get first and last name from user input
+            // Get their favorite number
+            // Get their favorite animal
+
+            // Once you have gotten all their info and stored it,
+            // display it back to them.
+
+            // If their favorite number is greater than 10, display it in green.
+            // If their favorite number is less than 5, display it in red.
+        }
+
+    internal class num1
+    {
+    }
+}
+}
             
 
            
@@ -78,20 +95,3 @@ namespace DisplayInfo
 
 
 
-
-            // Get first and last name from user input
-            // Get their favorite number
-            // Get their favorite animal
-
-            // Once you have gotten all their info and stored it,
-            // display it back to them.
-
-            // If their favorite number is greater than 10, display it in green.
-            // If their favorite number is less than 5, display it in red.
-        }
-
-    internal class num1
-    {
-    }
-}
-}
